d2127f768e1333ce4bc5a7802261869646c3f17b
geleigh <57293957+geleigh@users.noreply.github.com>
2020-04-08 19:02:29 -0400

    updating
    updating after tutor

--- Missions-to-Mars/scrape-mars.py
+++ Missions-to-Mars/scrape-mars.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
-pip install splinter
-
-
-# In[22]:
-
-
 from bs4 import BeautifulSoup
 import pandas as pd
 from splinter import Browser
@@ -20,38 +11,18 @@ import time
 
 
 # # MARS NEWS
-
-# In[23]:
-
-
 # url
 url = 'https://mars.nasa.gov/news/'
-
-
-# In[24]:
-
 
 # Retrieve page with the requests module
 response = requests.get(url)
 
-
-# In[25]:
-
-
 # Create BeautifulSoup object; parse with 'lxml'
 soup = BeautifulSoup(response.text, 'lxml')
-
-
-# In[26]:
-
 
 # Executable Path/Initialize Browser
 executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
 browser = Browser("chrome", **executable_path, headless=False)
-
-
-# In[27]:
-
 
 # Scrape the Latest News Title
 news_title = soup.find("div", class_="content_title").get_text()
@@ -64,8 +35,6 @@ print(news_p)
 
 # # MARS IMAGES
 
-# In[51]:
-
 
 # JPL Mars Space Images - Featured Image
 
@@ -74,17 +43,10 @@ browser = Browser("chrome", **executable_path, headless=False)
 url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
 browser.visit(url)
 
-
-# In[52]:
-
-
 ## Use splinter to navigate the site and find the image url for the current Featured Mars Image and assign the url string to a variable called `featured_image_url`.
 
 full_image_button = browser.find_by_id("full_image")
 full_image_button.click()
-
-
-# In[53]:
 
 
 # Find More Info button to click
@@ -92,16 +54,9 @@ browser.is_element_present_by_text("more info", wait_time=1)
 more_info = browser.find_link_by_partial_text("more info")
 more_info.click()
 
-
-# In[54]:
-
-
 # Parse Results HTML with BeautifulSoup
 html = browser.html
 image_soup = BeautifulSoup(html, "html.parser")
-
-
-# In[59]:
 
 
 # Image URL
@@ -111,11 +66,6 @@ print(featured_image_url)
 
 
 # # MARS WEATHER
-
-# In[88]:
-
-
-## 
 
 html = requests.get("https://twitter.com/marswxreport?lang=en").text
 weather_soup = BeautifulSoup(html, "html.parser")
@@ -134,9 +84,6 @@ except:
 
 # # MARS FACTS
 
-# In[95]:
-
-
 # Visit the Mars Facts webpage [here](https://space-facts.com/mars/) and use Pandas to scrape the table containing facts about the planet including Diameter, Mass, etc.
 mars_df = pd.read_html("https://space-facts.com/mars/")[0]
 # print(mars_df)
@@ -146,19 +93,12 @@ print(mars_df.to_html())
 
 
 # # MARS HEMISPHERES
-
-# In[96]:
-
-
 # Visit the USGS Astrogeology site [here](https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars) to obtain high resolution images for each of Mar's hemispheres.
 
 executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
 browser = Browser("chrome", **executable_path, headless=False)
 url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
 browser.visit(url)
-
-
-# In[101]:
 
 
 hemisphere_image_urls = []
@@ -180,10 +120,5 @@ for item in range(len(links)):
     browser.back()
     
 hemisphere_image_urls
-
-
-# In[ ]:
-
-
 
 
